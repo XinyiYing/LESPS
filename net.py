ebd54e5b31daefe8e3b42def355814695ec304af
Xinyi Ying <yingxinyi18@nudt.edu.cn>
2023-06-18 10:54:24 +0800

    Update net.py

--- net.py
+++ net.py
@@ -39,6 +39,7 @@ class Net(nn.Module):
     def update_gt(self, pred, gt_mask, thresh_Tb, thresh_k, size):
         bs, c, feat_h, feat_w = pred.shape
         background_length = 33
+        target_length = 3
         gt_mask_pos = (gt_mask>0.5).float()
         updated_gt_mask = gt_mask.clone()
         
@@ -48,10 +49,11 @@ class Net(nn.Module):
             cur_point_mask = pred.new_zeros(bs, c, feat_h, feat_w)
             cur_point_mask[0, 0, int(region.centroid[0]), int(region.centroid[1])] = 1
             nbr_mask = ((F.conv2d(cur_point_mask, weight=torch.ones(1,1,background_length,background_length).to(gt_mask.device), stride=1, padding=background_length//2))>0).float()
+            targets_mask = ((F.conv2d(cur_point_mask, weight=torch.ones(1,1,target_length,target_length).to(gt_masks.device), stride=1, padding=target_length//2))>0).float()
             pred_nbr = pred.clone() * nbr_mask
             gt_mask_nbr = gt_mask.clone() * nbr_mask
             
-            ### Cadidate Pixels Extraction
+            ### Candidate Pixels Extraction
             max_limitation = size[0] * size[1] * 0.0015
             adaptive_thresh = (pred_nbr ).max()* (thresh_Tb + thresh_k*(1-thresh_Tb)*((len(region.coords)/max_limitation)).to(gt_mask.device))
             thresh_mask = (pred_nbr > adaptive_thresh).float()
@@ -61,14 +63,14 @@ class Net(nn.Module):
             if label_image.max() > 1:
                 for num_cur in range(label_image.max()):
                     curr_mask = thresh_mask * torch.tensor(label_image == (num_cur + 1)).float().unsqueeze(0).to(gt_mask.device) 
-                    if (curr_mask * gt_mask_pos).sum() == 0:
+                    if (curr_mask * targets_mask).sum() == 0:
                         thresh_mask = thresh_mask - curr_mask
             
             ### Average Weighted Summation
             updated_gt_mask_nbr = gt_mask_nbr * (1-thresh_mask) + ((gt_mask_nbr  + pred_nbr)/2)* thresh_mask
             updated_gt_mask = torch.max(updated_gt_mask, updated_gt_mask_nbr)
         
-        ### Ensure initinal GT point label
+        ### Ensure initial GT point label
         updated_gt_mask = torch.max(updated_gt_mask, (gt_mask==1).float())
 
         return updated_gt_mask
