7a8edb99c661c4c5c1c394fb66f79846534eca72
Xinyi Ying <yingxinyi18@nudt.edu.cn>
2024-06-16 00:10:24 +0800

    Add files via upload

--- dataset.py
+++ dataset.py
@@ -160,6 +160,33 @@ class TestSetLoader(Dataset):
     def __len__(self):
         return len(self.test_list) 
 
+class InferenceSetLoader(Dataset):
+    def __init__(self, dataset_dir, train_dataset_name, test_dataset_name, img_norm_cfg=None):
+        super(InferenceSetLoader).__init__()
+        self.dataset_dir = dataset_dir
+        with open(self.dataset_dir+'/img_idx/test_' + test_dataset_name + '.txt', 'r') as f:
+            self.test_list = f.read().splitlines()
+        if img_norm_cfg == None:
+            self.img_norm_cfg = get_img_norm_cfg(train_dataset_name, dataset_dir)
+        else:
+            self.img_norm_cfg = img_norm_cfg
+            
+    def __getitem__(self, idx):
+        img = Image.open(self.dataset_dir + '/images/' + self.test_list[idx] + '.png').convert('I')
+        
+        img = Normalized(np.array(img, dtype=np.float32), self.img_norm_cfg)
+            
+        h, w = img.shape
+        times = 32
+        img = np.pad(img, ((0, (h//times+1)*times-h),(0, (w//times+1)*times-w)), mode='constant')
+        
+        img = img[np.newaxis,:]
+        
+        img = torch.from_numpy(np.ascontiguousarray(img))
+        return img, [h,w], self.test_list[idx]
+    def __len__(self):
+        return len(self.test_list) 
+
 class augumentation(object):
     def __call__(self, input, target):
         if random.random()<0.5:
--- inference.py
+++ inference.py
@@ -39,7 +39,7 @@ if opt.img_norm_cfg_mean != None and opt.img_norm_cfg_std != None:
   opt.img_norm_cfg['std'] = opt.img_norm_cfg_std
 
 def test(): 
-    test_set = TestSetLoader(opt.dataset_dir, opt.train_dataset_name, opt.test_dataset_name, opt.img_norm_cfg)
+    test_set = InferenceSetLoader(opt.dataset_dir, opt.train_dataset_name, opt.test_dataset_name, opt.img_norm_cfg)
     test_loader = DataLoader(dataset=test_set, num_workers=1, batch_size=1, shuffle=False)
     
     net = Net(model_name=opt.model_name, mode='test').cuda()
@@ -49,14 +49,10 @@ def test():
     eval_mIoU = mIoU() 
     eval_PD_FA = PD_FA()
     with torch.no_grad():
-        for idx_iter, (img, gt_mask, size, img_dir) in enumerate(test_loader):
+        for idx_iter, (img, size, img_dir) in enumerate(test_loader):
             img = Variable(img).cuda()
             pred = net.forward(img)
             pred = pred[:,:,:size[0],:size[1]]
-            gt_mask = gt_mask[:,:,:size[0],:size[1]]
-            eval_mIoU.update((pred>opt.threshold).cpu(), gt_mask)
-            eval_PD_FA.update((pred[0,0,:,:]>opt.threshold).cpu(), gt_mask[0,0,:,:], size)   
-            
             ### save img
             model_name = opt.pth_dir.split('/')[-1] .split('.')[0]
             if opt.save_img == True:
@@ -64,29 +60,17 @@ def test():
                 if not os.path.exists(opt.save_img_dir + opt.test_dataset_name + '/' + model_name ):
                     os.makedirs(opt.save_img_dir + opt.test_dataset_name + '/' + model_name )
                 img_save.save(opt.save_img_dir + opt.test_dataset_name + '/' + model_name + '/' + img_dir[0] + '.png')  
-        
-        results1 = eval_mIoU.get()
-        results2 = eval_PD_FA.get()
-        print("pixAcc, mIoU:\t" + str(results1))
-        print("PD, FA:\t" + str(results2))
-        opt.f.write("pixAcc, mIoU:\t" + str(results1) + '\n')
-        opt.f.write("PD, FA:\t" + str(results2) + '\n')
 
 if __name__ == '__main__':
-    opt.f = open('./test_' + (time.ctime()).replace(' ', '_').replace(':', '_') + '.txt', 'w')
     for pth_dir in opt.pth_dirs:
         opt.train_dataset_name = pth_dir.split('/')[0]
         print(pth_dir)
-        opt.f.write(pth_dir + '\n')
         for dataset_name in opt.dataset_names:
             opt.test_dataset_name = dataset_name
             opt.pth_dir = opt.save_log + pth_dir
             print(opt.test_dataset_name)
-            opt.f.write(opt.test_dataset_name + '\n')
             for model_name in opt.model_names:
                 if model_name in pth_dir:
                     opt.model_name = model_name
             test()
         print('\n')
-        opt.f.write('\n')
-    opt.f.close()
